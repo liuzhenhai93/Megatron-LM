0d96afd5fbcef3ae672a7b34c713371ee5da4a8a
liuzhenhai <liuzhenhai93@outlook.com>
2023-05-24 23:36:28 +0800

    polish

--- test_sparse_attention.py
+++ test_sparse_attention.py
@@ -2,15 +2,19 @@ import torch
 import time
 from megatron.model.sparse_attention import XformerSparseAttention as SparseAttention
 
-if __name__ == "__main__":
-    BATCH = 1
-    TP = 8
-    HEADS = 96 // TP
-    SEQ = 32768
-    EMB = 12288 // TP
-    BLOCK_SIZE = 16
-    DROPOUT = 0.0
-    attention = SparseAttention(block_size=BLOCK_SIZE, seq_len = 32768, num_heads = HEADS)
+
+
+BATCH = 1
+TP = 8
+HEADS = 64 // TP
+SEQ = 4*1024
+EMB = 8192 // TP
+DROPOUT = 0.1
+
+
+def test(block_size):
+    print(f"block size{block_size}")
+    attention = SparseAttention(block_size=block_size, seq_len = SEQ, num_heads = HEADS)
     shape = (BATCH, HEADS, SEQ, EMB // HEADS)  
     dtype=torch.bfloat16
     q = torch.randn(*shape, dtype=dtype).cuda()
@@ -21,14 +25,18 @@ if __name__ == "__main__":
     v.requires_grad_(True)
 
     def func(iteration):
+        out_grad = torch.randn(*shape).to(dtype).to("cuda:0")
         torch.cuda.synchronize()
         start_t = time.time()
         for i in range(iteration): 
             out = attention(q, k, v) 
-            out.backward(torch.randn((*out.shape)).to(out.dtype).to("cuda:0"))
+            #out.backward(out_grad)
         torch.cuda.synchronize()
         end_t = time.time()
         return (end_t - start_t) / iteration
 
-    func(10)
     print(func(10))
+    
+if __name__ == "__main__":
+    for block_size in [16, 32, 64, 128]:
+        test(block_size)    
