c6b54dd2385887865d9044500efb159a7f52c338
liuzhenhai <liuzhenhai93@outlook.com>
2023-05-24 09:59:22 +0800

    polish

--- megatron/model/sparse_attention.py
+++ megatron/model/sparse_attention.py
@@ -1,3 +1,5 @@
+import torch
+
 from xformers.components.attention import BlockSparseAttention
 from xformers.components.attention.sparsity_config import FixedSparsityConfig
 
@@ -27,35 +29,3 @@ class XformerSparseAttention(MegatronModule):
 
     def forward(self, q, k, v):
         return self._att(q, k, v)
-
-
-if __name__ == "__main__":
-    BATCH = 1
-    TP = 8
-    HEADS = 96 // TP
-    SEQ = 32768
-    EMB = 12288 // TP
-    BLOCK_SIZE = 16
-    DROPOUT = 0.0
-    print(blocks)
-    attention = XformerSparseAttention(seq_len = 32768, num_heads = HEADS)
-    shape = (BATCH, HEADS, SEQ, EMB // HEADS)  
-    q = torch.randn(*shape, dtype=dtype).cuda()
-    q.requires_grad_(True)
-    k = torch.randn(*shape, dtype=dtype).cuda()
-    k.requires_grad_(True)
-    v = torch.randn(*shape, dtype=dtype).cuda()
-    v.requires_grad_(True)
-
-    def func(iteration):
-        torch.cuda.synchronize()
-        start_t = time.time()
-        for i in range(iteration): 
-            out = attention(q, k, v) 
-            out.backward(torch.randn((*out.shape)).to(out.dtype).to("cuda:0"))
-        torch.cuda.synchronize()
-        end_t = time.time()
-        return (end_t - start_t) / iteration
-
-    func(10)
-    print(func(10))
---
+++ test_sparse_attention.py
@@ -0,0 +1,33 @@
+import torch
+from megatron.model.sparse_attention import XformerSparseAttention as SparseAttention
+
+if __name__ == "__main__":
+    BATCH = 1
+    TP = 8
+    HEADS = 96 // TP
+    SEQ = 32768
+    EMB = 12288 // TP
+    BLOCK_SIZE = 16
+    DROPOUT = 0.0
+    print(blocks)
+    attention = SparseAttention(seq_len = 32768, num_heads = HEADS)
+    shape = (BATCH, HEADS, SEQ, EMB // HEADS)  
+    q = torch.randn(*shape, dtype=dtype).cuda()
+    q.requires_grad_(True)
+    k = torch.randn(*shape, dtype=dtype).cuda()
+    k.requires_grad_(True)
+    v = torch.randn(*shape, dtype=dtype).cuda()
+    v.requires_grad_(True)
+
+    def func(iteration):
+        torch.cuda.synchronize()
+        start_t = time.time()
+        for i in range(iteration): 
+            out = attention(q, k, v) 
+            out.backward(torch.randn((*out.shape)).to(out.dtype).to("cuda:0"))
+        torch.cuda.synchronize()
+        end_t = time.time()
+        return (end_t - start_t) / iteration
+
+    func(10)
+    print(func(10))
